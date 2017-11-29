3257785baab47718b7daa0cb5e6ea34a274161d6
omlet94 <n.omieczynski@gmail.com>
2017-11-29 21:28:22 +0100

    działającze wczytywanie i zapis RGB

--- project.c
+++ project.c
@@ -2,61 +2,133 @@
 #include <string.h>
 #include <malloc.h>
 
-unsigned char *read_bmp(char *fname,int* _w, int* _h)
-{
-    unsigned char head[54];
-    FILE *f = fopen(fname,"rb");
-
-    // BMP header is 54 bytes
-    fread(head, 1, 54, f);
-
-    //int w = head[18] + ( ((int)head[19]) << 8) + ( ((int)head[20]) << 16) + ( ((int)head[21]) << 24);
-	int w = *(int*)&head[18];
-    //int h = head[22] + ( ((int)head[23]) << 8) + ( ((int)head[24]) << 16) + ( ((int)head[25]) << 24);
-	int h = *(int*)&head[22];
-    // lines are aligned on 4-byte boundary
-    int lineSize = (w / 8 + (w / 8) % 4);
-    int fileSize = lineSize * h;
-
-    unsigned char *img = malloc(w * h);
-	unsigned char *data = malloc(fileSize);
-
-    // skip the header
-    fseek(f,54,SEEK_SET);
-
-    // skip palette - two rgb quads, 8 bytes
-    fseek(f, 8, SEEK_CUR);
-
-    // read data
-    fread(data,1,fileSize,f);
-
-    // decode bits
-    int i, j, k, rev_j;
-    for(j = 0, rev_j = h - 1; j < h ; j++, rev_j--) {
-        for(i = 0 ; i < w / 8; i++) {
-            int fpos = j * lineSize + i, pos = rev_j * w + i * 8;
-            for(k = 0 ; k < 8 ; k++)
-                img[pos + (7 - k)] = (data[fpos] >> k ) & 1;
-        }
-    }
-
-    free(data);
-    *_w = w; *_h = h;
-    return img;
-}
+//unsigned char *read_bmp(char *fname,int* _w, int* _h)
+//{
+//    unsigned char header[54];
+//    FILE *fd = fopen(fname,"rb");
+//	if (fd == NULL)
+//	{
+//		printf("Error: fopen failed\n");
+//		return 1;
+//	}
+//	// Read header
+//	fread(header, sizeof(unsigned char), 54, fd);
+//	int width = (int)&header[18];
+//	int height = (int)&header[22];
+//	int padding;
+//	// Calculate padding
+//	while ((width * 3 + padding) % 4 != 0)
+//	{
+//		padding++;
+//	}
+//
+//	// Compute new width, which includes padding
+//	int widthnew = width * 3 + padding;
+//	unsigned char *texels = (unsigned char *)malloc(width * height * sizeof(unsigned char));
+//	if (texels == NULL)
+//	{
+//		printf("Error: Malloc failed. when you try to allocate bmp memory\n");
+//		return 1;
+//	}
+//	// Allocate temporary memory to read widthnew size of data
+//	unsigned char* data = (unsigned char *)malloc(widthnew * sizeof(unsigned int));
+//	// Read row by row of data and remove padded data.
+//	for (int i = 0; i < height; i++)
+//	{
+//		// Read widthnew length of data
+//		fread(data, sizeof(unsigned char), widthnew, fd);
+//
+//		// Retain width length of data, and swizzle RB component.
+//		// BMP stores in BGR format, my usecase needs RGB format
+//		for (int j = 0; j < width * 3; j += 3)
+//		{
+//			int index = (i * width + j/3);
+//			if (data[j + 0] == 0 && data[j + 1] == 0 && data[j + 2] == 0) {
+//				int pixel = 0;
+//				texels[index] = (unsigned char)pixel;
+//			}
+//			else {
+//				int pixel = 1;
+//				texels[index] = (unsigned char)pixel;
+//			}
+//		}
+//	}
+//	free(data);
+//	fclose(fd);
+//    *_w = width; *_h = height;
+//    return texels;
+//}
 
 int main()
 {
-    int w, h, i, j;
-    unsigned char* img = read_bmp("test1.bmp", &w, &h);
-
-    for(j = 0 ; j < h ; j++)
+    /*int width, height, i, j;
+    unsigned char* img = read_bmp("test1.bmp", &width, &height);
+	printf("width = %d\nheight = %d\n",width,height);
+    for(j = 0 ; j < height ; j++)
     {
-        for(i = 0 ; i < w ; i++)
-            printf("%c ", img[j * w + i] ? '0' : '1' );
-
+       for(i = 0 ; i < width ; i++)
+          printf("%c ", img[j * width + i]);
         printf("\n");
-    }
+   }*/
+	char* inputfilename = "test1.bmp";
+	static unsigned char *texels;
+	static int width, height;
+	FILE *fd;
+	fd = fopen(inputfilename, "rb");
+	if (fd == NULL)
+	{
+		printf("Error: fopen failed\n");
+		return 1;
+	}
 
+	unsigned char header[54];
+
+	// Read header
+	fread(header, sizeof(unsigned char), 54, fd);
+	width = *(int*)&header[18];
+	height = *(int*)&header[22];
+	int padding;
+	// Calculate padding
+	while ((width * 3 + padding) % 4 != 0)
+	{
+		padding++;
+	}
+
+	// Compute new width, which includes padding
+	int widthnew = width * 3 + padding;
+	texels = (unsigned char *)malloc(width * height * 3 * sizeof(unsigned char));
+	if (texels == NULL)
+	{
+		printf("Error: Malloc failed. when you try to allocate bmp memory\n");
+		return 1;
+	}
+	// Allocate temporary memory to read widthnew size of data
+	unsigned char* data = (unsigned char *)malloc(widthnew * sizeof(unsigned int));
+	// Read row by row of data and remove padded data.
+	for (int i = 0; i < height; i++)
+	{
+		// Read widthnew length of data
+		fread(data, sizeof(unsigned char), widthnew, fd);
+
+		// Retain width length of data, and swizzle RB component.
+		// BMP stores in BGR format, my usecase needs RGB format
+		for (int j = 0; j < width * 3; j += 3)
+		{
+			int index = (i * width * 3) + (j);
+			texels[index + 0] = data[j + 0];//r -> B
+			texels[index + 1] = data[j + 1];//g -> G
+			texels[index + 2] = data[j + 2];//b -> R
+		}
+	}
+
+	free(data);
+	fclose(fd);
+	
+	FILE *fo;
+	fo = fopen("test1output.bmp", "w");
+	fwrite(header, sizeof(unsigned char), 54 * sizeof(unsigned char), fo);
+	fwrite(texels, sizeof(unsigned char), width * height * 3 * sizeof(unsigned char), fo);
+
+	fclose(fo);
     return 0;
 }

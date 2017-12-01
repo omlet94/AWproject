e45f39188ad15af488b7a8338f47f5d972332b0c
omlet94 <n.omieczynski@gmail.com>
2017-12-01 05:02:24 +0100

    fixed bugs with malloc
    added erosion dilation and opening

--- project.c
+++ project.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <omp.h>
 
 unsigned char *read_bmp(char *inputfilename, unsigned char header[], int* _width, int* _height)
 {
 	static unsigned char *img;
-	int width, height;
+	static int width, height;
 	int padding;
 	FILE *fd;
 	fd = fopen(inputfilename, "rb");
@@ -40,11 +40,10 @@ unsigned char *read_bmp(char *inputfilename, unsigned char header[], int* _width
 			img[index + 2] = data[j + 2];//b -> R
 		}
 	}
-
-	free(data);
-	fclose(fd);
 	*_width = width;
 	*_height = height;
+	free(data);
+	fclose(fd);
 	return img;
 }
 void write_bmp(char *outputfilename, unsigned char header[], unsigned char outputimg[], int* width, int* height) {
@@ -54,13 +53,14 @@ void write_bmp(char *outputfilename, unsigned char header[], unsigned char outpu
 	fwrite(outputimg, sizeof(unsigned char), (*width) * (*height) * 3 * sizeof(unsigned char), fo);
 	fclose(fo);
 }
-void show_info(char *inputfilename, int* width, int* height) {
+void show_info(char *inputfilename, int* width, int* height, int* struct_size) {
 	printf("Morphological Opening\n");
-	printf("============================\n");
+	printf("=====================================\n");
 	printf("File: %s\n", inputfilename);
-	printf("Width: %d\n", *width);
-	printf("Height: %d\n", *height);
-	printf("============================\n");
+	printf("Width: %d px\n", *width);
+	printf("Height: %d px\n", *height);
+	printf("Structural element size: %d x %d\n", *struct_size, *struct_size);
+	printf("=====================================\n");
 }
 unsigned char *dilation(unsigned char input_img[], int* width, int* height , int* struct_size)
 {	
@@ -83,16 +83,16 @@ unsigned char *dilation(unsigned char input_img[], int* width, int* height , int
 				for (int k = -struct_margin; k <= struct_margin; k++) {		
 					for (int l = -struct_margin; l <= struct_margin; l++) {							
 						index = centerpoint + k * ((*width) * 3) + l * 3;
-						if ((input_img[index] == (char)0) && (input_img[index + 1] == (char)0) && (input_img[index + 2] == (char)0)) {
-							output_img[centerpoint + 0] = (char)0;
-							output_img[centerpoint + 1] = (char)0;
-							output_img[centerpoint + 2] = (char)0;
+						if ((input_img[index] == 0) && (input_img[index + 1] == 0) && (input_img[index + 2] == 0)) {
+							output_img[centerpoint + 0] = 0;
+							output_img[centerpoint + 1] = 0;
+							output_img[centerpoint + 2] = 0;
 							goto end;
 						}
 						else {
-							output_img[centerpoint + 0] = input_img[centerpoint + 0];
-							output_img[centerpoint + 1] = input_img[centerpoint + 1];
-							output_img[centerpoint + 2] = input_img[centerpoint + 2];
+							output_img[centerpoint + 0] = 255;
+							output_img[centerpoint + 1] = 255;
+							output_img[centerpoint + 2] = 255;
 						}
 					}
 				}
@@ -106,25 +106,82 @@ unsigned char *dilation(unsigned char input_img[], int* width, int* height , int
 			}
 		}
 	}
-	printf("done!\n");
-	printf("%d\n", (int)output_img[*width*3+3] );
+	return output_img;
+}
+unsigned char *erosion(unsigned char input_img[], int* width, int* height, int* struct_size)
+{
+	static unsigned char *output_img;
+	output_img = (unsigned char *)malloc((*width) * (*height) * 3 * sizeof(unsigned char));
+	if (output_img == NULL)
+	{
+		printf("Error: Malloc failed. when you try to allocate bmp memory\n");
+		return NULL;
+	}
+	int struct_margin = (*struct_size - 1) / 2;
+	int index = 0;
+	int centerpoint = 0;
+	for (int i = 0; i < *height; i++)
+	{
+		for (int j = 0; j < (*width) * 3; j += 3)
+		{
+			centerpoint = (i * (*width) * 3) + (j);
+			if ((i >= struct_margin) && (i < (*height - struct_margin)) && (centerpoint >= (i * ((*width) * 3) + struct_margin * 3)) && (centerpoint < ((i + 1) * ((*width) * 3) - 3 - struct_margin * 3))) {
+				for (int k = -struct_margin; k <= struct_margin; k++) {
+					for (int l = -struct_margin; l <= struct_margin; l++) {
+						index = centerpoint + k * ((*width) * 3) + l * 3;
+						if ((input_img[index] == 255) && (input_img[index + 1] == 255) && (input_img[index + 2] == 255)) {
+							output_img[centerpoint + 0] = 255;
+							output_img[centerpoint + 1] = 255;
+							output_img[centerpoint + 2] = 255;
+							goto end;
+						}
+						else {
+							output_img[centerpoint + 0] = input_img[centerpoint + 0];
+							output_img[centerpoint + 1] = input_img[centerpoint + 1];
+							output_img[centerpoint + 2] = input_img[centerpoint + 2];
+						}
+					}
+				}
+			end:;
+			}
+			else
+			{
+				output_img[centerpoint + 0] = 255;
+				output_img[centerpoint + 1] = 255;
+				output_img[centerpoint + 2] = 255;
+			}
+		}
+	}
+	return output_img;
+}
+unsigned char *morph_open(unsigned char input_img[], int* width, int* height, int* struct_size) {
+	static unsigned char *output_img;
+	output_img = (unsigned char *)malloc((*width) * (*height) * 3 * sizeof(unsigned char));
+	if (output_img == NULL)
+	{
+		printf("Error: Malloc failed. when you try to allocate bmp memory\n");
+		return NULL;
+	}
+	output_img = dilation(erosion(input_img, width, height, struct_size), width, height, struct_size);
 	return output_img;
 }
 int main(int argc, char** argv)
 {
 	int width, height, i, j,struct_size;
-	struct_size = 3;
+	struct_size = 21;
 	char* inputfilename = "test1.bmp";
 	char* outputfilename = "test1_output.bmp";
 	unsigned char header[54];
 	unsigned char* input_img;
 	unsigned char* output_img;
-	//unsigned char* output_img;
+	input_img = (unsigned char *)malloc(width * height * 3 * sizeof(unsigned char));
+	output_img = (unsigned char *)malloc(width * height * 3 * sizeof(unsigned char));
 	input_img = read_bmp(inputfilename, header, &width, &height);
-	show_info(inputfilename, &width, &height);
-	output_img = dilation(input_img, &width, &height,&struct_size);
+	show_info(inputfilename, &width, &height, &struct_size);
+	output_img = morph_open(input_img, &width, &height,&struct_size);
 	write_bmp(outputfilename, header, output_img, &width, &height);
+	printf("Created Output File: %s\n", outputfilename);
 	free(input_img);
-	//free(output_img);
+	free(output_img);
 	return 0;
 }
